c831ab907344b2d5c94cdc43893f5c6a3648f491
stanley <chw058@gmail.com>
2018-01-08 21:38:14 -0800

    mvp

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -23,7 +23,7 @@ const AboutMe = (props) => {
                 <Segment
                 
                 textAlign='center'
-                style={{ minHeight: window.screenY, padding: '1em 0em', backgroundColor: '#00ACC1'}}
+                style={{ minHeight: document.body.clientHeight, padding: '1em 0em', backgroundColor: '#00ACC1'}}
                 vertical
                 >
 
